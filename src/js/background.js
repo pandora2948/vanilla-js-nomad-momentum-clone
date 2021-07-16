dccb1a884c6e5c4812929cd66130f6c2d8a3cb06
dongjae <pandora2948@naver.com>
2021-07-17 00:22:16 +0900

    feat:did it

--- src/js/background.js
+++ src/js/background.js
@@ -1,12 +1,15 @@
 const backGroundSection = document.querySelector("#backgroundImageSection");
 
 const randomArray = [
-  "#493a3e", "##423a3c", "#450d1b", "#653843"];
+  "0.png", "1.png", "2.png"];
 
 function randomSelector() {
   const randomNumber = Math.floor(Math.random() * randomArray.length);
   const selectedColor = randomArray[randomNumber];
-  backGroundSection.style.backgroundColor = selectedColor;
+  const createImg = document.createElement("img");
+  createImg.src = `/src/img/${randomNumber}.png`;
+  createImg.classList.add("background-img");
+  backGroundSection.appendChild(createImg);
 }
 
 randomSelector();
